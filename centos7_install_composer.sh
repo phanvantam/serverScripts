1ea4f1900454f70168c6f7a41181f7b56a00253b
Phan Văn Tâm <tampv.bg@gmail.com>
2022-11-15 11:17:38 +0700

    Create centos7_install_composer.sh

---
+++ centos7_install_composer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+
+HASH="$(wget -q -O - https://composer.github.io/installer.sig)"
+
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+
+composer
+
+php -r "unlink('composer-setup.php');"
