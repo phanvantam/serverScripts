8224c8413755ae049b11b882520633b977fb29a0
Phan Văn Tâm <tampv.bg@gmail.com>
2022-11-15 11:53:39 +0700

    Create centos7_install_php_7_4.sh

---
+++ centos7_install_php_7_4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum -y install https://rpms.remirepo.net/enterprise/remi-release-7.rpm
+
+sudo yum -y install yum-utils
+sudo yum-config-manager --enable remi-php74
+
+sudo yum update
+sudo yum install php php-cli
+sudo yum install php  php-cli php-fpm php-mysqlnd php-zip php-devel php-gd php-mcrypt php-mbstring php-curl php-xml php-pear php-bcmath php-json php-dom php-mbstring php-pdo php-gd php-mysql
