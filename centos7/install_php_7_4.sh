00b58dda3bcda94f0248c7014d839672d43d3310
Phan Văn Tâm <33310683+phanvantam@users.noreply.github.com>
2023-02-13 18:26:07 +0700

    Update install_php_7_4.sh

--- centos7/install_php_7_4.sh
+++ centos7/install_php_7_4.sh
@@ -6,6 +6,6 @@ sudo yum -y install https://rpms.remirepo.net/enterprise/remi-release-7.rpm
 sudo yum -y install yum-utils
 sudo yum-config-manager --enable remi-php74
 
-sudo yum update
-sudo yum install php php-cli
-sudo yum install php  php-cli php-fpm php-mysqlnd php-zip php-devel php-gd php-mcrypt php-mbstring php-curl php-xml php-pear php-bcmath php-json php-dom php-mbstring php-pdo php-gd php-mysql
+sudo yum -y update
+sudo yum -y install php php-cli
+sudo yum -y install php  php-cli php-fpm php-mysqlnd php-zip php-devel php-gd php-mcrypt php-mbstring php-curl php-xml php-pear php-bcmath php-json php-dom php-mbstring php-pdo php-gd php-mysql
