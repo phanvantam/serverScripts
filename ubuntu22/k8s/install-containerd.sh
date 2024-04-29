c730b546b0052cd567bd8c8701519c1ca90827d3
Phan Văn Tâm <33310683+phanvantam@users.noreply.github.com>
2024-04-29 17:37:51 +0000

    .

---
+++ ubuntu22/k8s/install-containerd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+sudo modprobe overlay
+sudo modprobe br_netfilter
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+sudo sysctl --system
+sudo apt install containerd -y
+mkdir /etc/containerd
+containerd config default > /etc/containerd/config.toml
+sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+systemctl restart containerd
---
+++ ubuntu22/k8s/install-k9s.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt install snapd
+snap install k9s
+alias k9s=/snap/k9s/current/bin/k9s
